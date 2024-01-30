b02c60511d4e2c40d8fd69d2d78ec423491c3a12
RohiniShejwal <rohiniwagh12@gmail.com>
2024-01-30 15:53:59 +0530

    commit

--- ProjectByDurgesh/src/main/java/com/BikkadIT/ProjectByDurgesh/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BikkadIT.ProjectByDurgesh.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BikkadIT.ProjectByDurgesh.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
--- ProjectByDurgesh/src/main/java/com/BikkadIT/ProjectByDurgesh/repositories/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BikkadIT.ProjectByDurgesh.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BikkadIT.ProjectByDurgesh.entities.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-
-}
